de822f264051cb532f4ed9480a00fcd8b7fa9c88
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-02-15 15:42:43 +0700

    Editted the leaderboard page

--- public/js/test.js
+++ public/js/test.js
@@ -6,7 +6,10 @@ const instruction = document.querySelector(".instruction");
 const startButton = document.querySelector(".start-quiz");
 const testContainer = document.querySelector(".container");
 const overlayMessage = document.querySelector(".overlay");
-const centeredMessage = document.querySelector(".centered-message")
+const centeredMessage = document.querySelector(".centered-message");
+const strongElement = document.createElement("strong")
+strongElement.innerText = "F";
+
 let warningTimes = 0;
 
 function makeFullscreen(){
@@ -28,12 +31,69 @@ function redirectToResult(result){
 
 function displayWarningMessage(message){
     overlayMessage.style.display = "flex";
-    centeredMessage.innerText = message
+    centeredMessage.innerText = message;
+    centeredMessage.appendChild(document.createTextNode(" Press "))
+    centeredMessage.appendChild(strongElement);
+    centeredMessage.appendChild(document.createTextNode(" To Re-Enter Fullscreen."))
+
+    
+}
+
+function warnToFullscreenChange(event){
+    if (!document.fullscreenElement) { 
+        warningTimes++; 
+        let warningStatement;
+        
+        
+        if (warningTimes === 1) {
+            warningStatement = `You exited the screen. This is the first warning!
+            `;
+            
+        } else if (warningTimes === 2) {
+            warningStatement = `
+            You exited the screen. This is the second and last warning!
+            `;
+            
+        } else {
+            document.removeEventListener("fullscreenchange", warnToFullscreenChange)
+            return redirectToResult("suspended") 
+        }
+        
+        displayWarningMessage(warningStatement)
+    }
+}
+function setTimer(hours, minutes, seconds) {
+    
+    var distance = hours * 3600 + minutes * 60 + seconds;
+    timer.innerText = (hours.toString().length === 1 ? "0" + hours : hours) + ":" +
+            (minutes.toString().length === 1 ? "0" + minutes : minutes) + ":" +
+            (seconds.toString().length === 1 ? "0" + seconds : seconds); 
+    
+    var x = setInterval(function() {
+        distance -= 1;
+        var remainingHours = Math.floor(distance / 3600);
+        var remainingMinutes = Math.floor((distance % 3600) / 60);
+        var remainingSeconds = Math.floor(distance % 60);
+
+        
+
+        if (distance <= 0) {
+            document.removeEventListener("fullscreenchange", warnToFullscreenChange)
+            clearInterval(x);
+           
+            form.submit();
+        }
+        timer.innerText =
+            (remainingHours.toString().length === 1 ? "0" + remainingHours : remainingHours) + ":" +
+            (remainingMinutes.toString().length === 1 ? "0" + remainingMinutes : remainingMinutes) + ":" +
+            (remainingSeconds.toString().length === 1 ? "0" + remainingSeconds : remainingSeconds);
+        
+    }, 1000);
 }
 
 
 document.addEventListener('paste', (event) => {
-    redirectToResult("suspended")
+    event.preventDefault();
    
 });
 document.addEventListener('visibilitychange', () => {
@@ -52,54 +112,10 @@ startButton.addEventListener("click", () => {
 } 
 )
 
-document.addEventListener("fullscreenchange", (event) => {
-    if (!document.fullscreenElement) { 
-        warningTimes++; 
-        let warningStatement;
-        
-        
-        if (warningTimes === 1) {
-            warningStatement = 'You exited the screen. This is the first warning!';
-            
-        } else if (warningTimes === 2) {
-            warningStatement = 'You exited the screen. This is the second and last warning!';
-            
-        } else {
-           
-            return redirectToResult("suspended") 
-        }
-        
-        displayWarningMessage(warningStatement)
-    }
-});
+document.addEventListener("fullscreenchange", warnToFullscreenChange);
 
 
-function setTimer(hours, minutes, seconds) {
-    
-    var distance = hours * 3600 + minutes * 60 + seconds;
-    timer.innerText = (hours.toString().length === 1 ? "0" + hours : hours) + ":" +
-            (minutes.toString().length === 1 ? "0" + minutes : minutes) + ":" +
-            (seconds.toString().length === 1 ? "0" + seconds : seconds); 
-    
-    var x = setInterval(function() {
-        distance -= 1;
-        var remainingHours = Math.floor(distance / 3600);
-        var remainingMinutes = Math.floor((distance % 3600) / 60);
-        var remainingSeconds = Math.floor(distance % 60);
 
-        timer.innerText =
-            (remainingHours.toString().length === 1 ? "0" + remainingHours : remainingHours) + ":" +
-            (remainingMinutes.toString().length === 1 ? "0" + remainingMinutes : remainingMinutes) + ":" +
-            (remainingSeconds.toString().length === 1 ? "0" + remainingSeconds : remainingSeconds);
-
-        if (distance < 0) {
-            clearInterval(x);
-        
-            form.submit();
-        }
-        
-    }, 1000);
-}
 
 
 
