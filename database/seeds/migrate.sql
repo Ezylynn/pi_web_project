ca6de132687024ad746f71389d8edfe1e69a8dcc
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-03-05 19:27:06 +0700

    added the pg json file

---
+++ database/seeds/migrate.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp
+);
