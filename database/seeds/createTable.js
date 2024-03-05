9ed5345ed1a2ae1a07bb7cc0c56c4e734abd06d5
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-03-05 18:48:20 +0700

    testing the database

--- database/seeds/createTable.js
+++ database/seeds/createTable.js
@@ -1,11 +1,19 @@
-const {pool} = require("../database")
 
-const client = await pool.connect()
+const createTable = async () => {
+    const {pool} = require("../database")
 
-client.query(`CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    role user_role NOT NULL,
-    created_at TIMESTAMP DEFAULT current_timestamp
-);`)
+
+    const client = await pool.connect()
+
+    await client.query(`CREATE TABLE users (
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role user_role NOT NULL,
+        created_at TIMESTAMP DEFAULT current_timestamp
+    );`)
+
+    await client.release()
+}
+
+createTable()
