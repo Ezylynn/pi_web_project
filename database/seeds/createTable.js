44ffa217ad4f9514ee413161e8b522584da652a6
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-03-05 18:50:29 +0700

    fixing stuff

--- database/seeds/createTable.js
+++ database/seeds/createTable.js
@@ -9,7 +9,6 @@ const createTable = async () => {
         user_id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
         password_hash VARCHAR(255) NOT NULL,
-        role user_role NOT NULL,
         created_at TIMESTAMP DEFAULT current_timestamp
     );`)
 
